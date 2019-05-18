590ef0bfe2bec19715a9d1b9b6cf0ea038f9bbd0
Guilherme Henrique <progr.guilherme@gmail.com>
2019-05-17 23:02:05 -0300

    Updates

---
+++ precache-manifest.6de08160ea77aab458aa41a023bd9fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d002512adc4acc30860c7941bfedd0db",
+    "url": "/dm-challenge/static/media/logo.d002512a.svg"
+  },
+  {
+    "revision": "ea5f9ea401919bbe1ad9",
+    "url": "/dm-challenge/static/js/runtime~main.cbb96fe8.js"
+  },
+  {
+    "revision": "0da76df9c55c20ceaa67",
+    "url": "/dm-challenge/static/js/main.98aaa122.chunk.js"
+  },
+  {
+    "revision": "b7207c601d19dc3a997b",
+    "url": "/dm-challenge/static/js/2.b3123782.chunk.js"
+  },
+  {
+    "revision": "0da76df9c55c20ceaa67",
+    "url": "/dm-challenge/static/css/main.892bb280.chunk.css"
+  },
+  {
+    "revision": "c61b463ee24641a773d1e4b80c6929de",
+    "url": "/dm-challenge/index.html"
+  }
+];
